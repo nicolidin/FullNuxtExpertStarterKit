f1088a3e9c49fd00c2973159a0048179e3221974
nicolas <nicolas.lidin@gmail.com>
2026-02-03 16:22:35 +0100

    feat: adapt .storybook/preview.ts to make storybook work

--- vue-lib-expert-starter-kit/.storybook/preview.ts
+++ vue-lib-expert-starter-kit/.storybook/preview.ts
@@ -2,34 +2,33 @@ import "./storybook.scss";
 
 import { createLidinAppKit } from "../src/lidinAppKitConfig/createLidinAppKit";
 import { DEFAULT_VUETIFY_CONFIG } from "../src/lidinAppKitConfig/vuetifyConfig/defaultVuetifyConfig";
-import { defineComponent } from "vue";
-import Layout from "../src/components/Organisms/Layout/Layout.vue";
+import HeaderLayout from "../src/components/Molecules/HeaderLayout/HeaderLayout.vue";
 import { setup } from "@storybook/vue3";
 import { createPinia } from "pinia";
 
-/* Core CSS required for Ionic components to work properly */
-
-/* Basic CSS for apps built with Ionic */
-export default defineComponent({
-  components: { Layout },
-});
-
-/* Optional CSS utils that can be commented out */
-
 setup((app) => {
   const pinia = createPinia();
-  app.use(pinia); // Register Pinia globally
+  app.use(pinia);
   const lidinAppKit = createLidinAppKit(DEFAULT_VUETIFY_CONFIG);
   app.use(lidinAppKit);
 });
 
+/** Layout pour les stories : v-app + HeaderLayout (sans sidebar) + zone contenu */
 export const decorators = [
   (story, context) => {
     const id = String(context.id || context.title || "").toLowerCase();
     if (id.includes("sidebartags")) {
       return { template: "<story />", components: { story } };
     }
-    return { components: { story, Layout }, template: "<Layout><story/></Layout>" };
+    return {
+      components: { story, HeaderLayout },
+      template: `
+        <v-app>
+          <HeaderLayout>Storybook</HeaderLayout>
+          <v-main><story /></v-main>
+        </v-app>
+      `,
+    };
   },
 ];
 
